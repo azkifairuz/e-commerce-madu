d462bc78817dcf27d538603f41a2fe30e086ec46
azkifairuz <azkiajmal@gmail.com>
2023-07-06 00:54:34 +0700

    set alias path src

--- src/components/atom/labelCategory.vue
+++ src/components/atom/labelCategory.vue
@@ -1,12 +1,6 @@
-<script setup>
-const props = defineProps({
-  category:String
-})
-</script>
-
 <template>
   <div class="w-24 h-10 py-2 px-5 rounded-md shadow-lg bg-yellow-hover 
   text-white hover:text-slate-600 hover:bg-yellow-main">
-    {{ props.category }}
+    <slot></slot>
   </div>
 </template>
--- src/pages/customer/layout/Customer.vue
+++ src/pages/customer/layout/Customer.vue
@@ -1,9 +1,15 @@
 <script setup>
-import Navbar from "../../../components/organism/Navbar.vue";
-import Jbtr from "../../../components/atom/JumboTron.vue";
+import Navbar from "@/components/organism/Navbar.vue";
+import Jbtr from "@/components/atom/JumboTron.vue";
+import LabelCategory from "@/components/atom/labelCategory.vue";
+import Api from "@/config/api/Api"
+
+
+
 </script>
 <template>
   <Navbar/>
   <Jbtr/>
+  <label-category />
   <router-view></router-view>
 </template>
