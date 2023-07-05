d46cb8c2897425b04b67e577b8af21b5915740e6
azkifairuz <azkiajmal@gmail.com>
2023-07-06 03:04:46 +0700

    create card component

---
+++ src/components/molecular/CardProduct.vue
@@ -0,0 +1,25 @@
+<script setup>
+const props = defineProps({
+  imageUrl: String,
+  title: String,
+  description: String,
+  category: String,
+  price: String,
+});
+</script>
+
+<template>
+  <div class="max-w-md mx-auto rounded overflow-hidden shadow-lg">
+    <img :src="imageUrl" :alt="title" class="w-full h-48 object-cover">
+    <div class="px-6 py-4">
+      <div class="font-bold text-xl mb-2">{{ title }}</div>
+      <p class="text-gray-700 text-base">{{ description }}</p>
+    </div>
+    <div class="px-6 pt-4 pb-2">
+      <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">{{ category }}</span>
+      <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">{{ price }}</span>
+    </div>
+  </div>
+</template>
+
+
--- src/components/organism/Navbar.vue
+++ src/components/organism/Navbar.vue
@@ -15,7 +15,7 @@ const links = [
 <template>
   <nav class="bg-yellow-main">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-      <div class="flex items-center justify-between h-16">
+      <div class="flex items-center justify-center h-16">
         <div class="flex items-center gap-5">
           <div class="flex-shrink-0">
             <img class="h-8 w-8" src="@/assets/logo.png" alt="Logo" />
