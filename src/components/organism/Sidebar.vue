a0899d5acf9cadf121a9cbb527f01a32f7f21a68
azkifairuz <azkiajmal@gmail.com>
2023-07-12 15:27:39 +0700

    udpate menu server

--- src/components/organism/Sidebar.vue
+++ src/components/organism/Sidebar.vue
@@ -4,12 +4,24 @@ import { useRouter } from "vue-router";
 const router = useRouter();
 const listItem = [
   {
-    name: "product",
-    title: "Produk",
+    name: "category",
+    title: "Jenis Madu",
   },
   {
-    name: "category",
-    title: "Kategori Produk",
+    name: "product",
+    title: "Madu",
+  },
+  {
+    name: "dataCustomer",
+    title: "Pelanggan",
+  },
+  {
+    name: "employee",
+    title: "Pegawai",
+  },
+  {
+    name: "selling",
+    title: "Penjualan",
   },
 ];
 function navigation(path) {
@@ -24,14 +36,14 @@ function navigation(path) {
       <div
         class="flex-col fixed top-0 bottom-0 flex p-3 w-60 duration-300 bg-yellow-main shadow"
       >
-        <div class="space-y-3">
+        <div class="space-y-4">
           <div class="flex items-center justify-between">
             <h2 class="text-xl font-bold text-black">Dashboard</h2>
           </div>
 
-          <div class="flex-1">
-            <ul class="pt-2  space-y-1 text-sm" v-for="item in listItem" :key="item.name">
-              <li class="rounded-md text-lg cursor-pointer border border-black font-semibold hover:bg-yellow-hover">
+          <div class="flex-1 justify-evenly gap-10 mt-10 flex flex-col">
+            <ul class="  text-sm" v-for="item in listItem" :key="item.name">
+              <li class="rounded-md  text-lg cursor-pointer  font-semibold hover:bg-yellow-hover">
                 <a @click="navigation(item.name)" class="flex items-center p-2 space-x-3 rounded-md">
                   <span class="text-[black]">{{ item.title }}</span>
                 </a>
