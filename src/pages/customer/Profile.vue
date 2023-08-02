bad32417428412202c616844cd12544912168db7
azkifairuz <azkiajmal@gmail.com>
2023-08-02 15:03:47 +0700

    feat:profile page

--- src/pages/customer/Profile.vue
+++ src/pages/customer/Profile.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { reactive, onMounted } from "vue";
 import Api from "@/config/api/Api";
+import BtnComponent from "@/Components/atom/BtnComponent.vue";
 import { objectToData } from "@/utils/ObjectToData";
 import { calculateAge } from "@/utils/CalculateAge";
 const { GET } = Api();
@@ -25,20 +26,24 @@ async function getProfile() {
   objectToData(profile, data);
   console.log(profile.tgl_lahir);
 }
+
+function edit (){
+  return 
+}
 onMounted(() => {
   getProfile();
 });
 </script>
 
 <template>
-  <main class="flex gap-2 py-10 h-screen px-6 mt-10">
-    <aside class="w-1/3 bg-white rounded-lg h-[90%] shadow p-5 border-black">
+  <main class="flex flex-col md:flex-row gap-2 py-10 h-screen px-6 mt-10">
+    <aside class="w-full h-fit md:w-1/3 bg-white rounded-lg md:h-[90%] shadow p-5 border-black">
       <h1 class="text-2xl font-bold font-poppins capitalize">
         {{ profile.nm_pelanggan }}
       </h1>
       <h1 class="text-xl font-poppins">{{ profile.email }}</h1>
     </aside>
-    <main class="w-full bg-white rounded-lg h-[90%] shadow p-5 border-black">
+    <main class="w-full relative bg-white rounded-lg h-[90%] shadow p-5 border-black">
       <ul>
         <li>
           <h1 class="text-xl font-bold capitalize">Alamat:</h1>
@@ -59,6 +64,15 @@ onMounted(() => {
           </h1>
         </li>
       </ul>
+      <div class="absolute bottom-6 right-6">
+        <BtnComponent
+          label="Edit"
+          @someEvent="edit"
+          primary-color="bg-red-500"
+          hover-color="hover:bg-red-700"
+          text-color="text-white"
+        />
+      </div>
     </main>
   </main>
 </template>
